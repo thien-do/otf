fa8c9abca1627f47af1ec017cf8b0b2efe129d84
Thien Do <thien.do@employmenthero.com>
2020-01-19 22:44:53 +0700

    feat: add title and meta desc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import "styles/index.css";
+import Head from "next/head";
 
 interface Props {
   Component: React.FC<any>,
-  pageProps: object
+  pageProps: object,
 };
 
+const TITLE = "OTF Show";
+const DESCRIPTION = "An interactive showcase of OpenType's typographic features";
+
 const MyApp: React.FC<Props> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <>
+    <Head>
+      <title>{TITLE}</title>
+      <meta name="description" content={DESCRIPTION} />
+    </Head>
+    <Component {...pageProps} />
+  </>
 );
 
 export default MyApp;
