0a7c276601a33e245d5d786b78ab211349f0f972
Thien Do <dvkndn@gmail.com>
2020-04-07 15:53:46 +0700

    feat: separate suggested fonts

--- src/components/code/overview/font.tsx
+++ src/components/code/overview/font.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import fonts from "fonts";
 import { State, SetState } from "state";
-import Dropdown from "components/dropdown";
-
-const options = fonts.map(font => ({ value: font, label: font }));
+import Dropdown, { Option } from "components/dropdown";
 
 interface Props { state: State; setState: SetState; }
 
-const Font: React.FC<Props> = ({ state, setState }) => (
-  <Dropdown value={state.font} setValue={setState.font} options={options} />
-);
+const toOption = (font: string): Option => ({ value: font, label: font });
+
+const Font: React.FC<Props> = ({ state, setState }) => {
+  const supported = state.feature.fonts;
+  const options: Option[] = [
+    { label: "supported typefaces", value: "0", isHeading: true },
+    ...supported.map (toOption),
+    { label: "other typefaces", value: "1", isHeading: true },
+    ...fonts.filter(f => !supported.includes(f)).map(toOption),
+  ];
+  return <Dropdown value={state.font} setValue={setState.font} options={options} />;
+};
 
 export default Font;
