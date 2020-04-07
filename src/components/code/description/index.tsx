67309efd15ee54e7f1ea8654fe907430eef9e32e
Thien Do <5953369+thien-do@users.noreply.github.com>
2020-04-08 03:43:16 +0700

    fix: improve paragraph spacing

--- src/components/code/description/index.tsx
+++ src/components/code/description/index.tsx
@@ -6,6 +6,7 @@ import { Feature } from "features";
 import Fonts from "./fonts";
 import Texts from "./texts";
 import Related from "./related";
+import Usage from "./usage";
 
 const getName = (ft: Feature) => {
   return ft.family_code && ft.family_name
@@ -17,16 +18,13 @@ interface Props { state: State; }
 
 const Description: React.FC<Props> = ({ state }) => (
   <div className={Tw().text18().leading30().$()}>
-    <p className={Tw().whitespacePreLine().$()}>
-      <span className={Tw().fontSemibold().$()}>{getName(state.feature)}</span>
-      <span> {state.feature.description}</span>
-    </p>
-    <p className={Tw().mt18().$()}>
+    <Usage feature={state.feature} />
+    <p className={Tw().mt24().$()}>
       <Fonts fonts={state.feature.fonts} /><span> </span>
       <Texts texts={state.feature.texts} />
     </p>
     {state.feature.related.length > 0 &&
-      <p className={Tw().mt18().$()}><Related codes={state.feature.related} /></p>}
+      <p className={Tw().mt24().$()}><Related codes={state.feature.related} /></p>}
   </div >
 );
 
