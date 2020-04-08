9745afda032c0931f11208cc555d9f458d0fa59a
Thien Do <5953369+thien-do@users.noreply.github.com>
2020-04-08 18:52:02 +0700

    fix: rename about

--- src/components/main/header/index.tsx
+++ src/components/main/header/index.tsx
@@ -6,7 +6,7 @@ const tw = Tw().py36().borderB1().borderSolid().borderCbd().$();
 
 const links = [
   { href: "/", text: "otf.show" },
-  { href: "/about", text: "about" },
+  { href: "/what's this", text: "about" },
   { href: "https://github.com/dvkndn/otf.show", text: "github" },
 ];
 
