dedb181657a03d1e89a933d96631417f9a0e35fd
Thien Do <5953369+thien-do@users.noreply.github.com>
2020-04-10 21:08:29 +0700

    build: add ga

--- src/components/side/index.tsx
+++ src/components/side/index.tsx
@@ -9,7 +9,7 @@ const twTitle = Tw().py18().px36().text24().leading36().text718().$();
 const Side: React.FC = () => (
   <aside className={Tw().py18().px36().lt1280Px0().$()}>
     {featureGroups.map(({ type, label }, index) => (
-      <div className={index === 0 ? "" : Tw().mt18().$()}>
+      <div key={type} className={index === 0 ? "" : Tw().mt18().$()}>
         <p className={twTitle}>{label}</p>
         <FeatureList features={featureArr.filter(f => f.type === type)} />
       </div>
