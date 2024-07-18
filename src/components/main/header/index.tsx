9c2333ce094ed896a3a408e25809bef49f3641ff
Thien Do <5953369+thien-do@users.noreply.github.com>
2024-07-19 00:14:11 +0700

    fix: Update home link

--- src/components/main/header/index.tsx
+++ src/components/main/header/index.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import HeaderLink from "./link";
 
 const links = [
-  { href: "/", text: "otf" },
+  { href: "/", text: "otf.thien.do" },
   { href: "/about", text: "what's this" },
   { href: "https://github.com/thien-do/otf", text: "github" },
 ];
 
 const Header: React.FC = () => (
   <header className="py-36 border-b border-solid border-CBD">
-    <ul className="flex text-18 leading-24 font-semibold">
-      {links.map((link, index) => (
-        <li key={link.text} className={index !== 0 ? "ml-36" : ""}>
+    <ul className="flex flex-wrap gap-y-18 gap-x-36 text-18 leading-24 font-semibold">
+      {links.map((link) => (
+        <li key={link.text}>
           <HeaderLink href={link.href}>{link.text}</HeaderLink>
         </li>
       ))}
